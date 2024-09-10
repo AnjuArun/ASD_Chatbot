9223c889f8d82d340eada193f6c8a18b7669b791
SURYAJIT ITHIPARAMBIL SUDHEERAN <124421567+Suryajit27@users.noreply.github.com>
2024-09-10 16:24:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,6 @@ def predict_autism(data:asd_input):
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=10000)
     
 #uvicorn app:app --reload
